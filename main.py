542b199f4a9970847300998b948ee4b7af8ffcc9
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2023-06-08 02:09:33 +0000

    feat: Introduced a function to get any directory variable from environment after validating

--- main.py
+++ main.py
@@ -8,26 +8,39 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 
+def get_dir_from_env(
+    dir_env_var_key: str,
+) -> str:
+    """
+    Retrieves a directory path from an environment variable.
+
+    Parameters:
+        dir_env_var_key (str): The key of the environment variable.
+
+    Returns:
+        str: The directory path obtained from the environment variable.
+
+    Raises:
+        EnvironmentError: If the environment variable is not set.
+        NotADirectoryError: If the directory path does not exist.
+
+    """
+    dir_env_var_value = os.getenv(dir_env_var_key)
+
+    if not dir_env_var_value:
+        raise EnvironmentError(f"'{dir_env_var_key}'")
+    
+    if not os.path.exists(dir_env_var_value):
+        raise NotADirectoryError(f"'{dir_env_var_value}'")
+    
+    logging.info(f"found dir: '{dir_env_var_value}'")
+    
+    return dir_env_var_value
+
+
 def main():
-    PAN_CARD_IMAGES_DIR = os.getenv("PAN_CARD_IMAGES_DIR")
-
-    if not PAN_CARD_IMAGES_DIR:
-        raise EnvironmentError("'PAN_CARD_IMAGES_DIR'")
-
-    if not os.path.exists(PAN_CARD_IMAGES_DIR):
-        raise NotADirectoryError(PAN_CARD_IMAGES_DIR)
-    
-    logging.info(f"found pan card images dir: '{PAN_CARD_IMAGES_DIR}'")
-    
-    ARCHIVED_IMAGES_DIR = os.path.join(PAN_CARD_IMAGES_DIR, "archived-images")
-    
-    if not os.path.exists(ARCHIVED_IMAGES_DIR):
-        logging.info(f"'archived-images' dir doesn't exist within '{PAN_CARD_IMAGES_DIR}'")
-        logging.info(f"creating 'archived-images' dir within '{PAN_CARD_IMAGES_DIR}'")
-
-        os.mkdir(ARCHIVED_IMAGES_DIR)
-
-        logging.info(f"created 'archived-images' dir within '{PAN_CARD_IMAGES_DIR}'")
+    PAN_CARD_NEW_IMAGES_DIR = get_dir_from_env("PAN_CARD_NEW_IMAGES_DIR")
+    PAN_CARD_ARCHIVED_IMAGES_DIR = get_dir_from_env("PAN_CARD_ARCHIVED_IMAGES_DIR")
 
 
 if __name__ == "__main__":
