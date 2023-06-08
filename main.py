ac266145ab3ecd01c603598f78a588101bdb3e99
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2023-06-08 03:55:07 +0000

    feat: Introduced a function to all the new pan card images from dir

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import glob
+import sys
+from typing import List
 from dotenv import load_dotenv
 import os
 import logging
@@ -8,9 +11,7 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 
-def get_dir_from_env(
-    dir_env_var_key: str,
-) -> str:
+def get_dir_from_env(dir_env_var_key: str,) -> str:
     """
     Retrieves a directory path from an environment variable.
 
@@ -38,9 +39,50 @@ def get_dir_from_env(
     return dir_env_var_value
 
 
+def get_new_pan_card_images(pan_card_new_img_dir: str) -> List:
+    """
+    Retrieves a list of new PAN card image files from the specified directory.
+
+    Parameters:
+        pan_card_new_img_dir (str): The directory path where the PAN card images are located.
+
+    Returns:
+        List[str]: A list of file paths representing the new PAN card image files.
+
+    Raises:
+        None
+
+    """
+    if not len(os.listdir(pan_card_new_img_dir)) > 0:
+        logging.error(f"dir: '{pan_card_new_img_dir}' is empty")
+
+        return None
+
+    img_extns = [".jpg", ".JPG", ".png", ".PNG"]
+    
+    img_files = []
+
+    for ext in img_extns:
+        img_files.extend(glob.glob(os.path.join(pan_card_new_img_dir, '*' + ext)))
+
+    if not img_files:
+        logging.error(f"no new pan card image(s) found inside dir: '{pan_card_new_img_dir}'")
+
+        return None
+
+    logging.info(f"{len(img_files)} new pan card image(s) found inside dir: '{pan_card_new_img_dir}'")
+
+    return img_files
+
+
 def main():
-    PAN_CARD_NEW_IMAGES_DIR = get_dir_from_env("PAN_CARD_NEW_IMAGES_DIR")
-    PAN_CARD_ARCHIVED_IMAGES_DIR = get_dir_from_env("PAN_CARD_ARCHIVED_IMAGES_DIR")
+    PAN_CARD_NEW_IMG_DIR = get_dir_from_env("PAN_CARD_NEW_IMG_DIR")
+    PAN_CARD_ARCHIVED_IMG_DIR = get_dir_from_env("PAN_CARD_ARCHIVED_IMG_DIR")
+
+    new_pan_card_images = get_new_pan_card_images(PAN_CARD_NEW_IMG_DIR)
+
+    if not new_pan_card_images:
+        sys.exit()
 
 
 if __name__ == "__main__":
