28e74f9ad342c85b43e646408fcb7917e38277d1
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2023-06-08 08:09:11 +0000

    feat: Removed get_dir_from_env function in favor of introducing the get_env_vars function to validate whether the environment variables exist or not empty

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import logging
 import base64
+import httpx
 
 
 load_dotenv()
@@ -36,34 +37,6 @@ def get_converted_img_data(new_imgs_list: List) -> Dict:
             new_imgs_dict[new_imgs_list[index]] = new_imgs_dict[new_imgs_list[index]].decode("utf-8")
 
     return new_imgs_dict
-
-
-def get_dir_from_env(dir_env_var_key: str,) -> str:
-    """
-    Retrieves a directory path from an environment variable.
-
-    Parameters:
-        dir_env_var_key (str): The key of the environment variable.
-
-    Returns:
-        str: The directory path obtained from the environment variable.
-
-    Raises:
-        EnvironmentError: If the environment variable is not set.
-        NotADirectoryError: If the directory path does not exist.
-
-    """
-    dir_env_var_value = os.getenv(dir_env_var_key)
-
-    if not dir_env_var_value:
-        raise EnvironmentError(f"'{dir_env_var_key}'")
-    
-    if not os.path.exists(dir_env_var_value):
-        raise NotADirectoryError(f"'{dir_env_var_value}'")
-    
-    logging.info(f"found dir: '{dir_env_var_value}'")
-    
-    return dir_env_var_value
 
 
 def get_new_pan_card_images(pan_card_new_img_dir: str) -> List:
@@ -102,9 +75,63 @@ def get_new_pan_card_images(pan_card_new_img_dir: str) -> List:
     return img_files
 
 
+def get_env_vars(*args) -> Dict[str, str]:
+    """
+    Retrieves environment variables and returns them as a dictionary.
+
+    Args:
+        *args: Variable number of environment variable names.
+
+    Returns:
+        Dict[str, str]: A dictionary where keys are the environment variable names and values are their corresponding values.
+
+    Raises:
+        EnvironmentError: If an environment variable is not found or is empty.
+
+    Example usage:
+        env_vars = get_env_vars("VAR1", "VAR2", "VAR3")
+        print(env_vars)
+
+    Note:
+        The function assumes that the necessary environment variables are defined and accessible.
+
+    """
+    env_vars = {}
+
+    for arg in args:
+        env_vars[arg] = os.getenv(arg)
+
+        if not env_vars[arg]:
+            raise EnvironmentError(f"'{arg}'")
+
+    return env_vars
+
+
 def main():
-    PAN_CARD_NEW_IMGS_DIR = get_dir_from_env("PAN_CARD_NEW_IMGS_DIR")
-    PAN_CARD_ARCHIVED_IMGS_DIR = get_dir_from_env("PAN_CARD_ARCHIVED_IMGS_DIR")
+    env_vars = get_env_vars(
+        "PAN_CARD_NEW_IMGS_DIR",
+        "PAN_CARD_ARCHIVED_IMGS_DIR",
+        "X_RAPIDAPI_KEY",
+        "X_RAPIDAPI_HOST",
+        "PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_URI",
+        "PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_TASK_ID",
+        "PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_GROUP_ID"
+    )
+
+    PAN_CARD_NEW_IMGS_DIR = env_vars["PAN_CARD_NEW_IMGS_DIR"]
+    PAN_CARD_ARCHIVED_IMGS_DIR = env_vars["PAN_CARD_ARCHIVED_IMGS_DIR"]
+
+    if not os.path.exists(PAN_CARD_NEW_IMGS_DIR):
+        raise NotADirectoryError(f"'{PAN_CARD_NEW_IMGS_DIR}'")
+    
+    logging.info(f"found dir: '{PAN_CARD_NEW_IMGS_DIR}'")
+
+    X_RAPIDAPI_KEY = env_vars["X_RAPIDAPI_KEY"]
+    X_RAPIDAPI_HOST = env_vars["X_RAPIDAPI_HOST"]
+
+    PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_URI = env_vars["PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_URI"]
+    PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_TASK_ID = env_vars["PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_TASK_ID"]
+    PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_GROUP_ID = env_vars["PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_GROUP_ID"]
 
     new_pan_card_imgs_list = get_new_pan_card_images(PAN_CARD_NEW_IMGS_DIR)
 
@@ -114,6 +141,22 @@ def main():
     new_pan_card_imgs_dict = get_converted_img_data(new_pan_card_imgs_list)
 
     del new_pan_card_imgs_list
+
+    # resp = httpx.post(
+    #     url=PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_URI,
+    #     headers={
+    #         "content-type": "application/json",
+    #         "X-RapidAPI-Key": X_RAPIDAPI_KEY,
+    #         "X-RapidAPI-Host": X_RAPIDAPI_HOST
+    #     },
+    #     data={
+    #         "task_id": PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_TASK_ID,
+    #         "group_id": PAN_CARD_OCR_EXTRACTION_SCHEDULER_ENDPOINT_GROUP_ID,
+    #         "data": {
+    #             "document1": "https://5.imimg.com/data5/TP/US/MU/SELLER-51778781/pan-card-500x500.jpg"
+    #         }
+    #     }
+    # )
 
 
 if __name__ == "__main__":
